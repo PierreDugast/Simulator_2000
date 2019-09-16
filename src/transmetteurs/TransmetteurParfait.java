8abb030792ed6771bf841df2c54afdd7c41eaf7c
ROHE <arnaud.rohe@gmail.com>
2019-09-16 20:32:36 +0200

    Finalisation de la classe TransmetteurParfait.

--- src/transmetteurs/TransmetteurParfait.java
+++ src/transmetteurs/TransmetteurParfait.java
@@ -2,22 +2,28 @@ package transmetteurs;
 import information.Information;
 import information.InformationNonConforme;
 
-public class TransmetteurParfait<R,T> extends Transmetteur<R,T>{
+public class TransmetteurParfait<R,T> extends Transmetteur<R,T>
+{
 	
-	@Override
-	public void emettre() throws InformationNonConforme {
-	Information<T> info = (Information<T>) informationRecue;
-		
-		for(int i=0;i<destinationsConnectees.size();i++){
-			destinationsConnectees.get(i).recevoir(info);
-		}
-		informationEmise = info;	
+	public TransmetteurParfait()
+	{
+		super();
 	}
-	@Override
-	public void recevoir(Information<R> information) throws InformationNonConforme {
+	
+	public void recevoir(Information<R> information) throws InformationNonConforme
+	{
 		informationRecue=information;
 		emettre();
 	}
-
 	
+	public void emettre() throws InformationNonConforme 
+	{
+	Information<T> info = (Information<T>) informationRecue;
+		
+		for(int i=0;i<destinationsConnectees.size();i++)
+		{
+			destinationsConnectees.get(i).recevoir(info);
+		}
+		informationEmise = info;
+	}
 }
