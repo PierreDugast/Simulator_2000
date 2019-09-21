5192ca11acbea06d6ccb0213acab1e224c6461fe
ROHE <arnaud.rohe@gmail.com>
2019-09-21 16:23:48 +0200

    Ajout du packadge transmetteurTest
    début de la rédaction des classes de test

---
+++ src/transmetteurTests/EmetteurNrzTest.java
@@ -0,0 +1,33 @@
+package transmetteurTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+import static org.hamcrest.CoreMatchers.is;
+
+public class EmetteurNrzTest {
+
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	
+	@Before
+	public void setUp()
+	{
+		//TODO:
+	}
+
+	@After
+	public void tearDown()
+	{
+		//TODO:
+	}
+	
+	@Test
+	public void emettreTest()
+	{
+		//TODO:
+	}
+}
