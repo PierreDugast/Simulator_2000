6e785fcdc662b3649d5556ac73d64ecea3bc7258
jeremie1234 <55394667+jeremie1234@users.noreply.github.com>
2019-09-23 16:26:16 +0200

    Ajout emetteur et recepteur(incomplet)

--- src/transmetteurs/EmetteurNrzt.java
+++ src/transmetteurs/EmetteurNrzt.java
@@ -1,6 +1,7 @@
 package transmetteurs;
 
 import ExceptionsGlobales.AnalogicArgumentException;
+import information.Information;
 import information.InformationNonConforme;
 
 public class EmetteurNrzt <R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
@@ -13,7 +14,44 @@ public class EmetteurNrzt <R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 
 	@Override
 	public void emettre() throws InformationNonConforme {
-		// TODO Auto-generated method stub
+		Float [] emission = new Float[this.informationRecue.nbElements()*this.nbEchantillon];
+		
+		
+		int c=0;
+		int tier = nbEchantillon/3;
+		for(int i=0 ; i<informationRecue.nbElements() ; i++) {
+			int j=c;
+			if(informationRecue.iemeElement(i).equals(true)) {				
+				while(c<(j+this.nbEchantillon)) {
+					if (c<j+tier) {
+						emission[c] = (c-j)*amplitudeMax/tier;
+					} else if (c<j+2*tier){
+						emission[c] = amplitudeMax;
+					} else if (c<j+3*tier) {
+						emission[c] = amplitudeMax + (j-c)*amplitudeMax/tier;
+					}
+					c++;
+				}
+			} else {
+				while(c<(j+this.nbEchantillon)) {
+					emission[c] = amplitudeMin;
+					if (c<j+tier) {
+						emission[c] = (j-c)*amplitudeMin/tier;
+					} else if (c<j+2*tier){
+						emission[c] = amplitudeMin;
+					} else if (c<j+3*tier) {
+						emission[c] = amplitudeMin + (c-j)*amplitudeMin/tier;
+					}
+					c++;
+				}
+			}
+	  
+		}
+		this.informationEmise = new Information(emission);
+		
+		for(int j=0;j<destinationsConnectees.size();j++){
+			destinationsConnectees.get(j).recevoir(this.informationEmise);
+		}
 		
 	}
 
--- src/transmetteurs/RecepteurNrzt.java
+++ src/transmetteurs/RecepteurNrzt.java
@@ -1,6 +1,7 @@
 package transmetteurs;
 
 import ExceptionsGlobales.AnalogicArgumentException;
+import information.Information;
 import information.InformationNonConforme;
 
 public class RecepteurNrzt<R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
@@ -14,8 +15,25 @@ public class RecepteurNrzt<R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 
 	@Override
 	public void emettre() throws InformationNonConforme {
-		// TODO Auto-generated method stub
+		boolean [] emission = new boolean[this.informationRecue.nbElements()/nbEchantillon];
 		
+		int c=0;
+		int tier = nbEchantillon/3;
+		for(int i=0 ; i<informationRecue.nbElements() ; i++) {
+			if(i%nbEchantillon/2==0) {
+				if(informationRecue.iemeElement(i)>amplitudeMax/2) {
+					emission[c] = true;
+				}
+				if(informationRecue.iemeElement(i)<amplitudeMin/2) {
+					emission[c] = false;
+				}
+			}
+		}
+		this.informationEmise = new Information(emission);
+		
+		for(int j=0;j<destinationsConnectees.size();j++){
+			destinationsConnectees.get(j).recevoir(this.informationEmise);
+		}		
 	}
 	
 }
