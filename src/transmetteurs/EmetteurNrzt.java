a6efabd447a40f4fbaf834f0e9db0b61b0607feb
jeremie1234 <55394667+jeremie1234@users.noreply.github.com>
2019-09-28 12:40:43 +0200

    Recepteur finit / Emetteur commentaire à faire

--- src/transmetteurs/EmetteurNrzt.java
+++ src/transmetteurs/EmetteurNrzt.java
@@ -4,49 +4,108 @@ import ExceptionsGlobales.AnalogicArgumentException;
 import information.Information;
 import information.InformationNonConforme;
 
+/**
+ * 
+ * @author jérémie
+ *
+ * Classe permettant d'emettre des message de type NRZT. C'est à dire des messages qui ont leur premier tier 
+ * des échantillons pour 1 symbol qui augmente jusqu'à la valeur max linéairement puis le deuxième tier des
+ * échantillons reste à cette valeur puis le dernier tier diminue linéairement jusqu'à 0 si le symbol suivant
+ * est négatif. Pour un symbol négatif le premier tier diminue jusqu'à la valeur min et le dernier tier augmente
+ * jusqu'à 0 si le symbol qui suit est négatif. Si des symbols consécutif sont identique la valeur entre le 
+ * deuxième tier du premier symbol et le deuxième tier du dernier symbol consécutif la valeur reste constante. 
+ */
 public class EmetteurNrzt <R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 {
-
+	
+	/**
+	 * Constructeur permettant de créer un émetteur NRZT à partir du constructeur de transmetteur avec trois parammètres
+	 * @param nbEchantillon
+	 * @param amplitudeMax
+	 * @param amplitudeMin
+	 * @throws AnalogicArgumentException
+	 */
 	public EmetteurNrzt(int nbEchantillon, Float amplitudeMax, Float amplitudeMin) throws AnalogicArgumentException 
 	{
 		super(nbEchantillon, amplitudeMax, amplitudeMin);
 	}
 
-	@Override
+	/**
+	 * Méthode permettant de mettre en forme l'information reçu pour ensuite la réémettre. La mise en forme consiste à 
+	 * créer une information qui pour chaque symbole crée nbEchantillon. Le premier tier des échantillons tend linéairement
+	 * vers la valeur amplitudeMax si le symbole est true et amplitudeMin si le symbole est false. Le deuxième tier est constant
+	 * à la valeur amplitudeMax si le symbole est true et amplitudeMin si le symbole est false. Si le symbole suivant est différent 
+	 * du symbole courant le dernier tier tend vers 0 linéairement. Si le symbole précèdent est identique au courant la valeur reste 
+	 * constante sur le premier tier du suivant et sur le dernier tier du courant. 
+	 */
 	public void emettre() throws InformationNonConforme {
 		Float [] emission = new Float[this.informationRecue.nbElements()*this.nbEchantillon];
 		
-		
-		int c=0;
+		int compteurEchantillon=0;
 		int tier = nbEchantillon/3;
 		for(int i=0 ; i<informationRecue.nbElements() ; i++) {
-			int j=c;
-			if(informationRecue.iemeElement(i).equals(true)) {				
-				while(c<(j+this.nbEchantillon)) {
-					if (c<j+tier) {
-						emission[c] = (c-j)*amplitudeMax/tier;
-					} else if (c<j+2*tier){
-						emission[c] = amplitudeMax;
-					} else if (c<j+3*tier) {
-						emission[c] = amplitudeMax + (j-c)*amplitudeMax/tier;
+			int symbolCourant=compteurEchantillon;
+			if(informationRecue.iemeElement(i).equals(true) && informationRecue.iemeElement(i+1).equals(false)) {				
+				while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+					if (compteurEchantillon<symbolCourant+tier) {
+						emission[compteurEchantillon] = (compteurEchantillon-symbolCourant)*amplitudeMax/tier;
+					} else if (compteurEchantillon<symbolCourant+2*tier){
+						emission[compteurEchantillon] = amplitudeMax;
+					} else if (compteurEchantillon<symbolCourant+3*tier) {
+						emission[compteurEchantillon] = amplitudeMax + (symbolCourant-compteurEchantillon)*amplitudeMax/tier;
 					}
-					c++;
+					compteurEchantillon++;
 				}
-			} else {
-				while(c<(j+this.nbEchantillon)) {
-					emission[c] = amplitudeMin;
-					if (c<j+tier) {
-						emission[c] = (j-c)*amplitudeMin/tier;
-					} else if (c<j+2*tier){
-						emission[c] = amplitudeMin;
-					} else if (c<j+3*tier) {
-						emission[c] = amplitudeMin + (c-j)*amplitudeMin/tier;
+			} 
+			if(informationRecue.iemeElement(i).equals(false) && informationRecue.iemeElement(i+1).equals(true)) {
+				while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+					emission[compteurEchantillon] = amplitudeMin;
+					if (compteurEchantillon<symbolCourant+tier) {
+						emission[compteurEchantillon] = (symbolCourant-compteurEchantillon)*amplitudeMin/tier;
+					} else if (compteurEchantillon<symbolCourant+2*tier){
+						emission[compteurEchantillon] = amplitudeMin;
+					} else if (compteurEchantillon<symbolCourant+3*tier) {
+						emission[compteurEchantillon] = amplitudeMin + (compteurEchantillon-symbolCourant)*amplitudeMin/tier;
 					}
-					c++;
+					compteurEchantillon++;
 				}
 			}
-	  
+			if(informationRecue.iemeElement(i).equals(true) && informationRecue.iemeElement(i+1).equals(true)) {
+				if(informationRecue.iemeElement(i-1).equals(true)) {
+					while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+						emission[compteurEchantillon] = amplitudeMax;
+						compteurEchantillon++;
+					}
+				}
+				if(informationRecue.iemeElement(i-1).equals(false)) {
+					while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+						if (compteurEchantillon<symbolCourant+tier) {
+							emission[compteurEchantillon] = (compteurEchantillon-symbolCourant)*amplitudeMax/tier;
+						} else if (compteurEchantillon<symbolCourant+3*tier){
+							emission[compteurEchantillon] = amplitudeMax;
+						}
+					}
+				}
+			}
+			if(informationRecue.iemeElement(i).equals(false) && informationRecue.iemeElement(i+1).equals(false)) {
+				if(informationRecue.iemeElement(i-1).equals(false)) {
+					while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+						emission[compteurEchantillon] = amplitudeMin;
+						compteurEchantillon++;
+					}
+				}
+				if(informationRecue.iemeElement(i-1).equals(true)) {
+					while(compteurEchantillon<(symbolCourant+this.nbEchantillon)) {
+						if (compteurEchantillon<symbolCourant+tier) {
+							emission[compteurEchantillon] = (symbolCourant-compteurEchantillon)*amplitudeMin/tier;
+						} else if (compteurEchantillon<symbolCourant+3*tier){
+							emission[compteurEchantillon] = amplitudeMin;
+						}
+					}
+				}
+			}
 		}
+		
 		this.informationEmise = new Information(emission);
 		
 		for(int j=0;j<destinationsConnectees.size();j++){
--- src/transmetteurs/RecepteurNrzt.java
+++ src/transmetteurs/RecepteurNrzt.java
@@ -1,29 +1,55 @@
 package transmetteurs;
-
 import ExceptionsGlobales.AnalogicArgumentException;
 import information.Information;
 import information.InformationNonConforme;
 
+/**
+ * 
+ * @author jérémie
+ * Classe réalisant la reception pour des messages de types NRZT
+ * 
+ */
 public class RecepteurNrzt<R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 {
-
+	
+	/**
+	 * Constructeur du recepteur NRZT utilisant le constructeur des transmetteurs avec 3 paramètres
+	 * @param nbEchantillon
+	 * @param amplitudeMax
+	 * @param amplitudeMin
+	 * @throws AnalogicArgumentException
+	 */
 	public RecepteurNrzt(int nbEchantillon, Float amplitudeMax, Float amplitudeMin) throws AnalogicArgumentException 
 	{
 		super(nbEchantillon, amplitudeMax, amplitudeMin);
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	/**
+	 * Méthode permettant de mettre en forme le message reçu puis de l'envoyer. La mise en forme du message consiste 
+	 * à récupérer l'information reçu et 1 fois tous les nbEchantillons mettre dans le tableau soit true soit false 
+	 * en comparant la valeur de l'échantillon pris en compte à amplitudeMax et amplitudeMin. Si l'échantillon est 
+	 * supérieur à amplitudeMax/2 alors on met true dans le tableau et si l'échantillon est inférieur à amplitudeMin/2 
+	 * on met false dans le tableau. On crée ensuite une information avec en paramètre du constructeur le tableau de 
+	 * boolean créé que l'on met dans information Emise.
+	 */
 	public void emettre() throws InformationNonConforme {
+		
+		// Déclaration du tableau qui va contenir les booleans.
 		Boolean [] emission;
+		
+		// Initialisation du tableau
+		// Si le nombre d'échantillon est pair on utilise cette initialisation. Sinon on utilise celle du else.
+		// Ceci permet d'éviter d'avoir une taille de tableau qui n'est pas entière.
 		if (nbEchantillon%2==0) {
 			emission = new Boolean[this.informationRecue.nbElements()/nbEchantillon];
 		} else {
 			emission = new Boolean[this.informationRecue.nbElements()/(nbEchantillon+1)];
 		}
 		
-		
+		// Compteur permettant de savoir quel élèment du tableau doit être modifié
 		int c=0;
+		
+		// Remplissage du tableau de boolean en fonction des valeurs des échantillons de l'information reçu
 		for(int i=0 ; i<informationRecue.nbElements() ; i++) {
 			if(i%nbEchantillon/2==0) {
 				String elementI = informationRecue.iemeElement(i).toString();
@@ -34,11 +60,14 @@ public class RecepteurNrzt<R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 				if(valeurElementI<amplitudeMin/2) {
 					emission[c] = false;
 				}
+				c++;
 			}
 		}
 		
+		// Création de l'information contenant les valeur du tableau de boolean créé dans la boucle précèdente
 		this.informationEmise = new Information(emission);
 		
+		// Envoie de l'information mise en forme vers les destinations connectées
 		for(int j=0;j<destinationsConnectees.size();j++){
 			destinationsConnectees.get(j).recevoir(this.informationEmise);
 		}		
