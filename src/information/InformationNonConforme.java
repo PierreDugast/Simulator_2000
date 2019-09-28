e3488191b9adfcd34cce8e81a0517e37257d1681
jeremie1234 <55394667+jeremie1234@users.noreply.github.com>
2019-09-28 10:58:13 +0200

    indentation fait

--- src/information/InformationNonConforme.java
+++ src/information/InformationNonConforme.java
@@ -5,10 +5,10 @@ public class InformationNonConforme extends Exception {
     private static final long serialVersionUID = 1917L;
     
     public InformationNonConforme() {
-	super();
+    	super();
     }
    
     public InformationNonConforme(String motif) {
-	super(motif);
+    	super(motif);
     }
 }
