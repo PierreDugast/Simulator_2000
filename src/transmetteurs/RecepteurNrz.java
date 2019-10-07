e12422cc4ceb82bbc8e1c0fc3bbf37a02cd01740
jeremie1234 <55394667+jeremie1234@users.noreply.github.com>
2019-10-07 23:38:18 +0200

    Fonctionne

--- src/transmetteurs/RecepteurNrz.java
+++ src/transmetteurs/RecepteurNrz.java
@@ -18,39 +18,40 @@ public class RecepteurNrz<R,T> extends ConvertisseurAnalogiqueNumerique<R,T>
 
 	@Override
 	public void emettre() throws InformationNonConforme {
-		Boolean [] emission = new Boolean [this.informationRecue.nbElements()/this.nbEchantillon];
-		//compteur du nombre de "packet" de taille nbElement parcourus
-		int nbPacket = 0;
-		//compteur interne Ã  chaque packet
-		int i = 0;
-		//tant qu'il reste un element non parcouru dans la liste
-		while((nbPacket*this.nbEchantillon+i)<this.informationRecue.nbElements())
-		{
-			//tant que i < Ã  la taille d'un Ã©chantillon
-			while (i<this.nbEchantillon)
-			{
-				//Si la valeur de l'information actuelle est celle de l'amplitude max, c'est que la packet est forcÃ©ment un 1 logique.
-				if ((Float) this.informationRecue.iemeElement(nbPacket*this.nbEchantillon+i) >= (Float) seuil)
-				{
-					//Assigner la valeur dans la liste de sortie
-					emission[nbPacket] = true;
-			
-					
-			
-				}
-				else if ((Float) this.informationRecue.iemeElement(nbPacket*this.nbEchantillon+i) <= (Float) seuil) {
-					emission[nbPacket] = false;
-					
-				}
-				//Sinon, incrÃ©menter i
-				i++;
+		//Récupère la partie entière de la division par 3 du nombre d'échantillon
+		int tier = (int) Math.floor(nbEchantillon/3);
+		nbEchantillon= tier*3;
+		// Déclaration et initialisation du tableau qui va contenir les booleans.
+		Boolean [] emission = new Boolean[this.informationRecue.nbElements()/nbEchantillon];		
+				
+		// Compteur permettant de savoir quel élément du tableau doit être modifié
+		int symboleCourant=0;
+		int tierCourant=0;
+		float moyenneSignal=0;
+		float valeurElementI=0;
+				
+		// Remplissage du tableau de boolean en fonction des valeurs des échantillons de l'information reçu
+		for(int i=0 ; i<informationRecue.nbElements() ; i++) {
+			if(i%nbEchantillon==0) {
+				tierCourant=0;
 			}
-			nbPacket++;
-			i=0;
-			//et rebelotte
+			String elementI = informationRecue.iemeElement(i).toString();
+			valeurElementI = Float.parseFloat(elementI);
+			moyenneSignal=moyenneSignal+valeurElementI;
+			if (tierCourant==tier*3-1){
+				moyenneSignal=moyenneSignal/(tier+1);
+				if(moyenneSignal>(amplitudeMax+amplitudeMin)/2) {
+					emission[symboleCourant] = true;
+				}
+				if(moyenneSignal<(amplitudeMax+amplitudeMin)/2) {
+					emission[symboleCourant] = false;
+				}
+				symboleCourant++;
+			}
+			tierCourant++;
 		}
-		
-		//DÃ©clanchement de la methode recevoir des destinations connectes
+				
+		// Crï¿½ation de l'information contenant les valeur du tableau de boolean créé dans la boucle précèdente
 		this.informationEmise = new Information(emission);
 		for(int j=0;j<destinationsConnectees.size();j++)
 		{
